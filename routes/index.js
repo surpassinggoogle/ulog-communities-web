b69324d829bc4cbe7ba0bcff7e5dcda1b0627e12
East <eastmaels@gmail.com>
2018-10-29 23:34:01 +0900

    in progress edit overseer

--- routes/index.js
+++ routes/index.js
@@ -60,5 +60,48 @@ auth.connect(basic),
   console.log(req.body);
 });
 
+router.get('/overseers/edit/:userId', (req, res) => {
+  Overseer.findOne({name : req.params.userId})
+    .then((overseer) => {
+      res.render('overseers/edit', {
+        title: 'Edit Overseer',
+        data: overseer
+      });
+    })
+    .catch(() => { res.send('Sorry! Something went wrong.'); });
+});
+
+router.post('/overseers/edit/:userId',
+[
+  body('name')
+    .isLength({ min: 1 })
+    .withMessage('Please enter a name'),
+  body('tags')
+    .isLength({ min: 1 })
+    .withMessage('Please enter a tag'),
+  body('maxweight')
+    .isLength({ min: 1 })
+    .withMessage('Please enter a weight'),
+],
+auth.connect(basic), 
+(req, res) => {
+  const errors = validationResult(req);
+
+  if (errors.isEmpty()) {
+    const overseer = new Overseer(req.body);
+    const query = { name: req.body.name }
+    Overseer.findOneAndUpdate(query, { tags: req.body.tags, maxweight: req.body.maxweight})
+      .then(() => { res.redirect('/overseers'); })
+      .catch(() => { res.send('Sorry! Something went wrong.'); });
+  } else {
+    res.render('overseers/edit', {
+      title: 'Overseer edit form',
+      errors: errors.array(),
+      data: req.body,
+    });
+  }
+
+  console.log(req.body);
+});
 
 module.exports = router;
